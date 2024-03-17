8092ebc59313a8041a61a13b055a68104f620f57
BusterWarn <buster.hultgren.warn@gmail.com>
2024-03-17 07:21:13 +0100

    Interpereter MWE

--- src/Interpereter.hs
+++ src/Interpereter.hs
@@ -1,25 +1,39 @@
-module Interpereter (interperet) where
+module Interpereter (interperet, evaluateExpr) where
 
 import AbstractSyntaxTree
+import Parser
+import Scanner
 import Tokens
 
-interperet :: Ast -> Float
-interperet = undefined
+interperet :: String -> Float
+interperet code =
+  let ast@(Ast stmts) = parse . scanTokens $ code
+      results = map evaluateStmt stmts
+   in sum results
 
-evaluate :: Expr -> Float
-evaluate (LiteralExpr token@(TOKEN _ _ (NUM value) _)) = value
-evaluate (GroupingExpr expr) = evaluate expr
-evaluate (UnaryExpr (TOKEN unary _ _ _) expr) =
-  let right = evaluate expr
+evaluateStmt :: Stmt -> Float
+evaluateStmt (ExprStmt expr) = evaluateExpr expr
+evaluateStmt (PrintStmt expr) = undefined
+evaluateStmt (BlockStmt stmts) = undefined
+evaluateStmt (IfStmt expr stmt maybeStmt) = undefined
+evaluateStmt (WhileStmt expr stmt) = undefined
+evaluateStmt (VarDeclStmt tokenType token expr) = undefined
+evaluateStmt (ErrorStmt expr) = undefined
+
+evaluateExpr :: Expr -> Float
+evaluateExpr (LiteralExpr token@(TOKEN _ _ (NUM value) _)) = value
+evaluateExpr (GroupingExpr expr) = evaluateExpr expr
+evaluateExpr (UnaryExpr (TOKEN unary _ _ _) expr) =
+  let right = evaluateExpr expr
    in case unary of
         MINUS -> -right
         _ -> 0
-evaluate (BinaryExpr leftExpr (TOKEN binary _ _ _) rightExpr) =
-  let left = evaluate leftExpr
-      right = evaluate rightExpr
+evaluateExpr (BinaryExpr leftExpr (TOKEN binary _ _ _) rightExpr) =
+  let left = evaluateExpr leftExpr
+      right = evaluateExpr rightExpr
    in case binary of
         MINUS -> left - right
         PLUS -> left + right
         SLASH -> left / right
         STAR -> left * right
-evaluate _ = 0
+evaluateExpr _ = 0
